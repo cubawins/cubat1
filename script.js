a55ea4c355423744bb6e54293d98d6e8883bc318
cubawins <victorcuba11@gmail.com>
2024-05-21 20:20:47 -0300

    nuevo

---
+++ script.js
@@ -0,0 +1,20 @@
+function validacion()
+{
+    var  nombre = document.getElementById("nombre").value;
+    var  apellido = document.getElementById("apellido").value;
+    var  direccion = document.getElementById("direccion").value;
+    var  altdir = document.getElementById("altdir").value;
+    var  telefonofijo = document.getElementById("telefonofijo").value;
+    var  codigoarea1 = document.getElementById("codigoarea1").value;
+    var telefonomovil =document.getElementById("telefonomovil").value;
+    var  codigoarea2 = document.getElementById("codigoarea2").value;
+    var email =document.getElementById("email").value;
+    var  edad = document.getElementById("edad").value;
+    var  fecha = document.getElementById("fecha").value;
+    var  masculino = document.getElementById("masculino").value;
+    var  femenino = document.getElementById("femenino").value;
+    var  otro = document.getElementById("otro").value;
+    var  antiguedad = document.getElementById("antiguedad").value;
+    var  area = document.getElementById("area").value;
+
+    }
